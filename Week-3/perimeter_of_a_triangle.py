45609d2488552c9e4f41e3cdb4bf7c2bdaa53349
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-14 15:49:07 -0400

    perimeter of a triangle program

---
+++ Week-3/perimeter_of_a_triangle.py
@@ -0,0 +1,8 @@
+def main():
+    side_one = float(input("What is the length of side 1? "))
+    side_two = float(input("What is the length of side 2? "))
+    side_three = float(input("What is the length of side 3? "))
+    print("The perimeter of the triangle is " + str(side_one + side_two + side_three))
+
+if __name__ == '__main__':
+    main()
