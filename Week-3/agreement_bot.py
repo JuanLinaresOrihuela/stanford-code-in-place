12b0dd306af34e5bbd501a8ac750ac7c46d82bb4
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-14 14:31:20 -0400

    new program

---
+++ Week-3/agreement_bot.py
@@ -0,0 +1,6 @@
+def main():
+    animal = input("What's your favorite animal? ")
+    print("My favorite animal is also " + animal + "!")
+
+if __name__ == '__main__':
+    main()
---
+++ Week-3/how_old_are_they.py
@@ -0,0 +1,15 @@
+def main():
+    anton = 21
+    beth = 6 + anton
+    chen = beth + 20
+    drew = chen + anton
+    ethan = chen
+    
+    print("Anton age is " + str(anton) + ".")
+    print("Beth age is " + str(beth) + ".")
+    print("Chen age is " + str(chen) + ".")
+    print("Drew age is " + str(drew) + ".")
+    print("Ethan age is " + str(ethan) + ".")
+
+if __name__ == '__main__':
+    main()
