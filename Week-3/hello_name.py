0a92c52f878c337cab80b9a4d3d7cf3b229a884c
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-15 00:21:40 -0400

    week 3 python programs

---
+++ Week-3/hello_name.py
@@ -0,0 +1,6 @@
+def main():
+    NAME = input("What is your name? ")
+    print("Hello " + NAME)
+
+if __name__ == '__main__':
+    main()
---
+++ Week-3/mad_libs.py
@@ -0,0 +1,10 @@
+WIZARD = 'Karel'
+NUMBER_OF_FRUIT = 6174
+FRUIT = 'mangoes'
+
+def main():
+    print("There once was a wizard by the name of " + WIZARD + " who loved to eat " + FRUIT + ".")
+    print(WIZARD + " always kept a stash of " + str(NUMBER_OF_FRUIT) + " " + FRUIT + " in their house...")
+
+if __name__ == '__main__':
+    main()
