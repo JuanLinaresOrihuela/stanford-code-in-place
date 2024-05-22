d9f438067cc322e6df0e97dcf2161e52ccdc652c
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-22 19:51:02 -0400

    if else simple program

---
+++ Week-4/if_else.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+if num == 0:
+    print("Your number is 0")
+elif num > 0:
+    print("Your number is positive")
+elif num < 0:
+    print("Your number is negative")
