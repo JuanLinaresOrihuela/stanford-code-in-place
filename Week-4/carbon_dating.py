0133226e84b18fac3319c6665ab66b3f560697f4
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-22 19:44:45 -0400

    carbon dating program

--- Week-4/carbon_dating.py
+++ Week-4/carbon_dating.py
@@ -3,12 +3,13 @@ import math
 K = -8266.64
 
 def main():
-    calculate_age_single_sample()
+    while True:
+        calculate_age_single_sample()
     
 def calculate_age_single_sample():
-    percent_left = float(input("% of natural c14:"))
+    percent_left = float(input("% of natural c14: "))
     age = K * math.log(percent_left/100)
-    print("Sample is " + str(age) + " years old.")
+    print("\nSample is " + str(age) + " years old.")
     
 if __name__ == "__main__":
     main()
