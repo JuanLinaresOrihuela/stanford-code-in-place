67b84d71317abdfee7da7dc84bf33563ae655271
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-22 21:37:03 -0400

    guess the door program

---
+++ Week-4/guess_the_door.py
@@ -0,0 +1,19 @@
+# door = int(input("Door: "))
+
+# # While the input is invalid 
+# while door < 1 or door >3 :
+#     # Tell the user the input was invalid
+#     print("Invalid door!")
+#     # Ask for a new input
+#     door = int(input("Door: "))
+    
+#     prize = 4
+#     if door == 1:
+#         prize = 2 + 9 // 10 * 100
+#     elif door == 2:
+#         locked = prize % 2 != 0
+#         if not locked:
+#             prize += 6
+#     elif door == 3:
+#         for i in range(door):
+#             prize += i
