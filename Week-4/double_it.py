1ab86e3d57a3489f521ea3723c82d7182276ddad
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-05-25 23:41:35 -0400

    code in place image

---
+++ Week-4/double_it.py
@@ -0,0 +1,12 @@
+# def main():
+#     curr_value = int(input("Enter a number: "))
+#     curr_value = curr_value * 2
+#     print(curr_value)
+    
+#     while curr_value < 100:
+
+        
+# if __name__ == '__main__':
+#     main()
+
+# Need to work on this. Unfinished.
