880fb4669f535892fa555ca473f26038806d035f
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-06-17 07:40:34 -0400

    lists programs

---
+++ Week-6/contain_an_element.py
@@ -0,0 +1,10 @@
+my_list = [42,100,10]
+
+def main():
+    if 42 in my_list:
+        print("List has element!")
+    else:
+        print("Element not present")
+        
+if __name__ == '__main__':
+    main()
---
+++ Week-6/element_not_present.py
@@ -0,0 +1,10 @@
+my_list = [42,100,10]
+
+def main():
+    if -3 in my_list:
+        print("List has element!")
+    else:
+        print("Element not present")
+    
+if __name__ == '__main__':
+    main()
